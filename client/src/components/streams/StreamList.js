cd3cbd62d8b2e2198a011a0bcb8b07dfdca682e5
Cristopher <cristopher.correo@gmail.com>
2020-04-25 18:49:36 +0100

    Added correct link to stream list

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -26,7 +26,7 @@ class StreamList extends React.Component {
           {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content ">
-            <Link to="/stream/Show" className="header">{stream.title}</Link>
+            <Link to={`/stream/show/${stream.id}`} className="header">{stream.title}</Link>
             <div className="description">{stream.description}</div>
           </div>
         </div>
